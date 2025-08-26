e5abc4ced617ed9c62daac302047f1a9a7f7d2bc
ledoux25 <sanguojoseph17@gmail.com>
2025-08-25 21:46:16 -0400

    Initial Commit

---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Sanguo Joseph Ledoux on 8/25/25.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
